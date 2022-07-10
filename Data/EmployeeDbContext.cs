1f7813fd84f07ae675448f1527c3258512077f29
AnushaVallabhareddy <anushavallabhareddy@outlook.com>
2022-07-10 19:36:01 +0530

    Second Commit

---
+++ Data/EmployeeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GitApp.Models;
+
+namespace GitApp.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext (DbContextOptions<EmployeeDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GitApp.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ Models/Emps.cs
@@ -0,0 +1,25 @@
+﻿
+
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    namespace GitApp.Models
+{
+        [Table("Employee")]
+        public class Employee
+        {
+            [Key]
+            public int EId { get; set; }
+            public string EName { get; set; }
+
+            public string EDesing { get; set; }
+
+            public DateTime? EDOJ { get; set; }
+
+        }
+    }
+
